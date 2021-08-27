573decb8b22a87712b587e20f7452fb5d84395c2
Ravinder <rvnegi786@gmail.com>
2021-08-27 11:14:03 +0530

    save

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
+/** @format */
 
-const express = require("express");
-const connect = require("./configs/db");
-const path = require("path");
-
+const express = require('express');
+const connect = require('./configs/db');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 
-console.log(__dirname)
-app.set("views", path.join(__dirname , "views/"));
-app.set("view engine" , "ejs");
+console.log(__dirname);
+app.set('views', path.join(__dirname, 'views/'));
+app.set('view engine', 'ejs');
 
-app.use("/static", express.static(path.join(__dirname, "style")))
+app.use('/static', express.static(path.join(__dirname, 'style')));
+app.use('/static', express.static(path.join(__dirname, 'script')));
 
-const productController = require("./controllers/product.controller");
+const productController = require('./controllers/product.controller');
 
-app.use("/products",productController);
+app.use('/products', productController);
 
-app.listen("4321",async ()=>{
-    await connect()
-    console.log("listening to port 4321")
-})
+app.listen('4321', async () => {
+	await connect();
+	console.log('listening to port 4321');
+});
