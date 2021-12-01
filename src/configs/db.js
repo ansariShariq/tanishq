60ba36a979c4e7557fa37e90887da6517fa0f21f
Ravinder Singh Negi <rvnegi786@gmail.com>
2021-12-01 22:40:14 +0530

    make it better

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,12 +2,15 @@
 const mongoose = require("mongoose");
 
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://shariq:tanishq@001@cluster0.mk1qp.mongodb.net/tanishq?retryWrites=true&w=majority",{
-        useNewUrlParser :true,
-        useUnifiedTopology:true,
-        useCreateIndex:true,
-        useFindAndModify : false
-    })
+    return mongoose.connect(
+			"mongodb+srv://ravinder:ravinder@cluster0.mk1qp.mongodb.net/tanishq?retryWrites=true&w=majority",
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true,
+				useFindAndModify: false,
+			},
+		);
 }
 
 module.exports = connect;
--- src/server.js
+++ src/server.js
@@ -3,20 +3,20 @@
 const express = require('express');
 const connect = require('./configs/db');
 const path = require('path');
-
+const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 console.log(__dirname);
-app.set('views', path.join(__dirname, 'views/'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views/"));
+app.set("view engine", "ejs");
 
-app.use('/static', express.static(path.join(__dirname, 'style')));
-app.use('/static', express.static(path.join(__dirname, 'Script')));
+app.use("/static", express.static(path.join(__dirname, "style")));
+app.use("/static", express.static(path.join(__dirname, "Script")));
 
-const homeController = require('./controllers/home.controller');
-app.use('/home', homeController);
+const homeController = require("./controllers/home.controller");
+app.use(port, homeController);
 
 app.listen('4321', async () => {
 	await connect();
